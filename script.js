81564e38df82f224e215e4d7ad8fae33fab5388f
Marko Dankovich <dankovic.marko@yahoo.com>
2022-09-14 12:07:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+function stars(){
+    let count = 50;
+    let scene = document.querySelector('.container');
+    let i = 0;
+    while (i < count){
+      let star = document.createElement('i');
+      let x = Math.floor(Math.random() * window.innerWidth);
+
+      let duration = Math.random() * 1;
+      let h= Math.random() * 50;
+      star.style.left = x + 'px';
+      star.style.width = 1 + 'px';
+      star.style.height = h + 'px';
+      star.style.animationDuration = duration + 's'; 
+      
+      scene.appendChild(star);
+      i++;
+    }
+  }
+  stars();
+
+
+function setupTypewriter(t){
+  var HTML = t.innerHTML;
+
+  t.innerHTML = "";
+
+  var cursorPosition = 0,
+  tag = "",
+  writingTag = false,
+  tagOpen = false,
+  typeSpeed = 100,
+  tempTypeSpeed = 0;
+
+  var type = function(){
+    
+    if (writingTag === true){
+      tag += HTML[cursorPosition];    
+    }
+    
+    if (HTML[cursorPosition] === "<"){
+      tempTypeSpeed = 0;
+      if (tagOpen){
+        tagOpen = false;
+        writingTag = true;
+      }else{
+        tag = "";
+        tagOpen = true;
+        writingTag = true;
+        tag += HTML[cursorPosition];
+      }
+    }
+    if (!writingTag && tagOpen){
+      tag.innerHTML += HTML[cursorPosition];
+    }
+    if (!writingTag && !tagOpen){
+      if (HTML[cursorPosition] === " "){
+        tempTypeSpeed = 0;
+      }
+      else{
+        tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+      }
+      t.innerHTML += HTML[cursorPosition];
+    }
+    if (writingTag == true && HTML[cursorPosition] === ">"){
+      tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+      writingTag = false;
+      if(tagOpen){
+        var newSpan = document.createElement("span");
+        t.appendChild(newSpan);
+        newSpan.innerHTML = tag;
+        tag = newSpan.firstChild;
+      }
+    }
+    cursorPosition += 1;
+    if(cursorPosition < HTML.length - 1){
+      setTimeout(type, tempTypeSpeed);
+    }
+  };
+
+  return{
+    type:type
+  };
+}
+
+var typer = document.getElementById('typewriter');
+
+typewriter = setupTypewriter(typewriter);
+
+typewriter.type();
